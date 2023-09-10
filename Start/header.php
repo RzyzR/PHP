6871a076d1a8b21abfa8ff6d3a176e53f51798d0
RzyzR <52755789+RzyzR@users.noreply.github.com>
2023-09-10 12:22:17 -0500

    Create header.php

---
+++ Start/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Página en PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Encabezado de la Página</h1>
+        <!-- Otro contenido común del encabezado -->
+    </header>
