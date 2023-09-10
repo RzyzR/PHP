21f63a720c10b1b1d1b697b918af8fcbbf04b904
RzyzR <randyzaruma40@gmail.com>
2023-09-10 11:59:17 -0500

    Creando una pagina simple en PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Página en PHP</title>
+</head>
+<body>
+    <h1>Bienvenido a mi página en PHP</h1>
+
+    <?php
+        // Aquí puedes escribir código PHP
+        $mensaje = "Hola, mundo!";
+        echo $mensaje;
+    ?>
+
+</body>
+</html>
